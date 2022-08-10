fbe3dfc4268aeaf3d5679e251e1a0e30ac85f91f
kepya <christiankepya@gmail.com>
2022-08-10 13:50:24 +0100

    dashboard

--- src/app/Http/Controllers/ManageAdminController.php
+++ src/app/Http/Controllers/ManageAdminController.php
@@ -240,6 +240,38 @@ class ManageAdminController extends Controller
             return redirect()->back();
     }
 
+    public function dashboard(){
+
+        $alltoken = $_COOKIE['token'];
+        $alltokentab = explode(';', $alltoken);
+        $token = $alltokentab[0];
+        $tokentab = explode('=',$token);
+        $tokenVal = $tokentab[1];
+        $Authorization = 'Bearer '.$tokenVal;
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://172.17.0.2:4000/client/auth/dashboard',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization:'.$Authorization,
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $informations = json_decode($response, true);
+
+        curl_close($curl);
+
+        return view('Client/dashboard',['informations' => $informations, 'res' => $response]);
+    }
+
     public function viewCustomers(){
 
         $url = "http://172.17.0.2:4000/admin/auth/getClient";
